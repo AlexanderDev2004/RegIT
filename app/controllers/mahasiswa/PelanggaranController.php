9c5adcaff23236fe015e319206b0c4c131330203
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-12-07 23:25:02 +0700

    feat : membuat Pelanggaran Controller dan Model Untuk Mahasiswa

---
+++ app/controllers/mahasiswa/PelanggaranController.php
@@ -0,0 +1,12 @@
+<?php 
+
+class PelanggaranController {
+
+    public function index(){
+        // Memuat file view untuk halaman Pelanggaran
+        require_once './app/views/mahasiswa/Pelanggaran.php';
+    }
+    
+}
+
+?>
