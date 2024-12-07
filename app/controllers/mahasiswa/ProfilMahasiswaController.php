e8f01d8a5fea46118cb8fa6c0096ed2f8aef4cd4
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-12-08 00:08:39 +0700

    fork_tes : menambahakan ProfilMahasiswa dan juga modelnya (ini blm fix)

---
+++ app/controllers/mahasiswa/ProfilMahasiswaController.php
@@ -0,0 +1,12 @@
+<?php 
+
+class ProfilMahasiswaController {
+
+    public function index(){
+        // Memuat file view untuk halaman ProfilMahasiswa
+        require_once './app/views/mahasiswa/ProfilMahasiswa.php';
+    }
+    
+}
+
+?>
