232eae93fd652cca826a4c433d3f573fe649e383
Khip01[desk] <aakhifathallah24@gmail.com>
2024-12-08 23:29:18 +0700

    chore(path): fix import path typo
    path typo, fix undefined class, tidy up

--- app/controllers/mahasiswa/ProfilMahasiswaController.php
+++ app/controllers/mahasiswa/ProfilMahasiswaController.php
@@ -1,11 +1,16 @@
 <?php
+
+require_once './app/models/mahasiswa/ProfilMahasiswaModel.php';
+
 class ProfilMahasiswaController {
     public function index() {
+        session_start();
+
         // Ambil data profil mahasiswa dari database
-        $model = new MahasiswaModel();
+        $model = new ProfilMahasiswaModel();
         $data['profil'] = $model->getProfilMahasiswaByNIM($_SESSION['nim']); // Misalnya berdasarkan NIM di session
 
         // Kirim data ke view
-        require_once '../app/views/mahasiswa/Profile.php';
+        require_once './app/views/mahasiswa/Profile.php';
     }
 }
