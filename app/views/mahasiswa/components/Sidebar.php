a3620050b508e977b69f4fe9b4e2ffcebb9c382a
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-12-13 23:15:02 +0700

    tes_fix : delete h-screen hanya ujicoba aja sih

--- app/views/mahasiswa/components/Sidebar.php
+++ app/views/mahasiswa/components/Sidebar.php
@@ -1,6 +1,6 @@
 <!-- SideBar -->
-<div class="flex w-full mx-auto h-screen">
-        <div class="w-64 h-screen bg-[#132145] text-white flex flex-col items-center py-6 absolute top-0 h-screen">
+<div class="flex w-full mx-auto ">
+        <div class="w-64 h-screen bg-[#132145] text-white flex flex-col items-center py-6 absolute top-0 ">
             <div class="mb-6">
                 <div class="w-20 h-20 flex items-center justify-center rounded-lg mt-6">
                     <img src=".././public/LogoSide.png" alt="logo" class="w-20 h-20 object-contain">
