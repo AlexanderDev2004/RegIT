f2de8222876cc177b6783d7938de51059145c00e
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-12-17 21:08:58 +0700

    fix : Nyoba

--- app/models/dpa/DPPelanggaranModel.php
+++ app/models/dpa/DPPelanggaranModel.php
@@ -1,7 +1,7 @@
 <?php
 
-include __DIR__ . '/../../core/db_config.php';
-include __DIR__ . '/../Model.php';
+require_once __DIR__ . '/../../core/db_config.php';
+require_once __DIR__ . '/../Model.php';
 class DPPelanggaranModel {
     private $model;
 
